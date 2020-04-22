ac63ebdf4ce9e63698ec17a0fdb70c1c33a4f3ec
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-04-22 10:01:05 -0700

    1. move masterTicketList into state(TicketControl)

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -7,7 +7,8 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state={
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      masterTicketList: []
     };
   }
 
@@ -22,10 +23,10 @@ class TicketControl extends React.Component {
     let currentlyVisibleState = null;
     let buttonText = null;
     if(this.state.formVisibleOnPage){
-      currentlyVisibleState = <NewTicketForm />
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
       buttonText = "Return to Ticket List";
     } else {
-      currentlyVisibleState = <TicketList />
+      currentlyVisibleState = <TicketList ticketList={this.state.masterTicketList}/>
       buttonText = "Add Ticket";
     }
     return(
